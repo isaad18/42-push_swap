c99cdd2d620159fed00f4c842a03562171fa4dc2
Imad Saad <isaad@MAD.local>
2022-04-06 12:44:03 +0400

    l

--- push_swap.c
+++ push_swap.c
@@ -59,10 +59,6 @@ void	startshit(t_data *data)
 	if (data->j == 1)
 		etla3(data);
 	data->i = 0;
-	// if (data->j > 3)
-	// 	data->stack = malloc(sizeof(int) * (data->j));
-	// data->final = malloc(sizeof(int) * (data->j));
-	// data->test = malloc(sizeof(int) * (data->j));
 	if (data->j > 3)
 		data->stack = ft_calloc( (data->j) + 1, sizeof(int));
 	data->final = ft_calloc( (data->j) + 1, sizeof(int));
